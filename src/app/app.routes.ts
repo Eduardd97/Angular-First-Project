9479fe29ed333083a8bda8e372750f6a6a5dad61
Eduard <kozeychukk1997@gmail.com>
2024-06-02 16:24:39 +0300

    user-card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
 import { UsersListComponent } from './users/users-list/users-list.component';
+import { UserProfileComponent } from './users/user-profile/user-profile.component';
 
-export const routes: Routes = [{
-    path: "users",
+export const routes: Routes = [
+  {
+    path: 'users',
     component: UsersListComponent,
-}];
+    // children: [
+    //   {
+    //     path: ':user.id',
+    //     component: UserProfileComponent,
+    //   },
+    // ],
+  },
+  {
+    path: 'users/:user.id',
+    component: UserProfileComponent,
+  },
+];
