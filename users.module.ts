f4596ec3996f5e73d2e93cbb13d1e3812e1b5628
Eduard <kozeychukk1997@gmail.com>
2024-06-06 22:00:47 +0300

    user-form

--- users.module.ts
+++ users.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { UsersListComponent } from "./users-list/users-list.component";
-import { UsersListItemComponent } from "./users-list-item/users-list-item.component";
-import { RouterOutlet, RouterLink, RouterLinkActive } from "@angular/router";
-import { UserProfileComponent } from "./user-profile/user-profile.component";
-import { UsersService } from "./users.service";
-import { UserFormComponent } from "./user-form/user-form.component";
-import { ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersListComponent } from './src/app/users/users-list/users-list.component';
+import { UsersListItemComponent } from './src/app/users/users-list-item/users-list-item.component';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { UserProfileComponent } from './src/app/users/user-profile/user-profile.component';
+import { UsersService } from './users.service';
+import { UserFormComponent } from './src/app/users/user-form/user-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [
-        UsersListComponent,
-        UsersListItemComponent,
-        UserProfileComponent,
-        UserFormComponent,
-    ],
-    exports: [UsersListComponent, UserProfileComponent, UserFormComponent],
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
-    providers: [UsersService],
+  declarations: [
+    UsersListComponent,
+    UsersListItemComponent,
+    UserProfileComponent,
+    UserFormComponent,
+  ],
+  exports: [UsersListComponent, UserProfileComponent, UserFormComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    ReactiveFormsModule,
+  ],
+  providers: [UsersService],
 })
 export class UsersModule {}
