6e68a251e828a298590e537ea66254cf7a03b001
Eduard <kozeychukk1997@gmail.com>
2024-06-09 14:11:31 +0300

    user-form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
-import { UsersListComponent } from './users/src/app/users/users-list/users-list.component';
-import { UserProfileComponent } from './users/src/app/users/user-profile/user-profile.component';
-import { UserFormComponent } from './users/src/app/users/user-form/user-form.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { UserProfileComponent } from './users/user-profile/user-profile.component';
+import { UserFormComponent } from './users/user-form/user-form.component';
+
+
+
 
 export const routes: Routes = [
   {
