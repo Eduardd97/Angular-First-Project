c43e626f41b81f7f6dbff55118c63eb4ee9e2f60
Eduard <kozeychukk1997@gmail.com>
2024-06-16 17:43:34 +0300

    types(union-type)-uyily_types-generics

--- src/app/types.ts
+++ src/app/types.ts
@@ -81,11 +81,200 @@ const data2: DataType = {
 type MyBoolean = true | false;
 
 type UserWithDeclarations = DefaultUser & {
-    declarations: string[];
+  declarations: string[];
 };
 
 const user: UserWithDeclarations = {
-    declarations: [],
-    id: "1",
-    name: "name",
+  declarations: [],
+  id: '1',
+  name: 'name',
+};
+
+//----------------------------------------------------------- //
+
+interface PostInteface {
+  id: string;
+  title: string;
+  text: string;
+  liked: number;
 }
+
+interface DeletedPostInterface extends PostInteface {
+  isDeleted: boolean;
+}
+
+// extends
+// implements
+
+type Str = string;
+
+class MyClass implements PostInteface {
+  id = '';
+  liked = 1;
+  text = '';
+  title = '';
+}
+
+type A = {
+  a: number;
+};
+
+type B = A & { b: number };
+
+// & | - not works with inteface
+
+// UTILY TYPES
+
+type UsersData = {
+  users?: string[];
+  posts?: string[];
+};
+
+type RequiredType = Required<{ a?: number }>;
+
+const saveData = (usersData: Required<UsersData>) => {
+  // if (usersData.posts) {
+  //     // ...
+  // }
+  // if (usersData.users) {
+  //     // ...
+  // }
+};
+
+type PostDataType = {
+  title: string;
+  id: string;
+  likes: number;
+};
+
+const PostData: Partial<PostDataType> = {};
+
+// Partial  - makes all the fields of the object optional
+// Required - makes all the fileds of the object required
+
+type InvoiceType = {
+  address: string;
+  amount: number;
+  id: string;
+  customer: string;
+};
+
+const word: 'hello' | 'goodbuye' = 'hello';
+
+const mainInvoiceData: Pick<InvoiceType, 'amount' | 'customer' | 'id'> = {
+  amount: 1,
+  customer: '',
+  id: '',
+};
+
+const mainInvoiceDataNoCustomer: Omit<InvoiceType, 'customer' | 'id'> = {
+  address: '',
+  amount: 10,
+};
+
+// Pick - gets couple of fields from any type
+// Omit - removes couole of fileds from any type
+
+// Завдання:
+type Car = {
+  price: number;
+  color: string;
+  name: string;
+  speed: number;
+};
+
+type CarPriceOnlyType = Pick<Car, 'price'>;
+
+const showThePrice = (carWithPriceOnly: CarPriceOnlyType) => {
+  console.log(`The price of the car is ${carWithPriceOnly.price}`);
+};
+
+type CarWithSpeedAndNameType = Omit<Car, 'price' | 'color'>;
+
+const showTheSpeed = (carWithSpeedAndName: CarWithSpeedAndNameType) => {
+  console.log(
+    `The speed of the car ${carWithSpeedAndName.name} is ${carWithSpeedAndName.speed}`
+  );
+};
+
+// Типізувати аргументи функцій
+type CoordsType = {
+  x: number;
+  y: number;
+  z: number;
+};
+
+// Record<string, string>
+const coords: Record<'x' | 'y' | 'z', number> = {
+  x: 0,
+  y: 0,
+  z: 0,
+};
+
+function main(a: number) {
+  if (a > 10) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+// typeof ""
+// typeof 10
+// typeof main
+
+const mainResult: ReturnType<typeof main> = 0;
+
+const first = () => 11;
+
+const second = (firstResult: ReturnType<typeof first>) => {};
+
+const firstResult = first();
+second(firstResult);
+
+class UserClass {
+  name: string = '';
+  age: number = 0;
+}
+
+new UserClass();
+
+const aClassInstance: InstanceType<typeof UserClass> = {
+  name: '',
+  age: 10,
+};
+
+// Generic
+
+type Union<T1, T2> = T1 & T2;
+
+const coordA = { a: 10 };
+const coordB = { b: 11 };
+
+type CoordAType = { a: number };
+type CoordBType = { b: number };
+
+const coordAAndB: Union<CoordAType, CoordBType> = {
+  a: 1,
+  b: 2,
+};
+
+// Home Work
+
+// Завдання:
+// type MainType = Record<'a'|'b', string>
+// const obj: MainType = {a:'1', b: '1'}
+
+// const obj2 = {a: '2', b: '2', c:'2'};
+// const obj3 = {a: '3'}
+// const obj4 = {a: 4}
+// типізуйте obj,2,3,4, використовуючи похідні від MainType та Utility
+
+type MainType = Record<'a'|'b', string>
+const obj: MainType = {a:'1', b: '1'}
+
+const obj2: MainType & { c: string } = {a: '2', b: '2', c:'2'};
+const obj3: Pick<MainType, 'a'> = {a: '3'};
+const obj4: Partial<Record<'a', string | number>> = {a: 4};
+
+
